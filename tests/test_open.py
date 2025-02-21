b1484c7c5f91dcac77ef58426766d114c50e79cf
Min RK <benjaminrk@gmail.com>
2025-02-21 09:37:41 +0100

    basic mac, Windows implementations

--- tests/test_open.py
+++ tests/test_open.py
@@ -1,2 +1,47 @@
-def test_import():
-    import webbrowser_open  # noqa
+import os
+import shlex
+import sys
+import webbrowser
+from pathlib import Path
+from unittest import mock
+
+import webbrowser_open
+
+
+def test_default_browser():
+    browser = webbrowser_open.get_default_browser()
+    assert browser is not None
+    assert Path(browser).exists()
+    assert browser is not None
+    assert os.path.exists(shlex.split(browser)[0])
+    if sys.platform == "win32":
+        assert "%1" in browser
+
+
+def test_register():
+    opener = webbrowser_open.register()
+    assert opener is not None
+    assert webbrowser.get() is opener
+
+
+def test_register_browser_env():
+    with mock.patch.dict(os.environ, {"BROWSER": "temporary"}):
+        opener = webbrowser_open.register()
+    assert opener is not None
+    assert webbrowser.get() is not opener
+
+
+def test_open():
+    webbrowser_open.open("https://example.org")
+
+
+def test_register_open():
+    opener = webbrowser_open.register()
+    assert opener is not None
+    with mock.patch.object(
+        webbrowser_open._backend, "open_with_browser", return_value=True
+    ) as method:
+        webbrowser.open("https://example.org")
+    method.assert_called_once_with(
+        "https://example.org", webbrowser_open.get_default_browser()
+    )
---
+++ webbrowser_open/_win.py
@@ -0,0 +1,62 @@
+import shlex
+import sys
+from subprocess import run
+
+assert sys.platform == "win32"  # for mypy
+
+from winreg import (  # noqa:E402
+    HKEY_CLASSES_ROOT,
+    HKEY_CURRENT_USER,
+    OpenKey,
+    QueryValueEx,
+)
+
+
+def _registry_lookup(root_key: str, sub_key: str, value_name: str = "") -> str | None:
+    """Lookup a registry item
+
+    Returns None if no value could be read
+    """
+    try:
+        with OpenKey(root_key, sub_key) as key:
+            return QueryValueEx(key, value_name)[0]
+    except OSError:
+        return None
+    return None
+
+
+def get_default_browser() -> str | None:
+    """Get the command to launch the default browser
+
+    Returns None if not found
+    """
+    browser_id = _registry_lookup(
+        HKEY_CURRENT_USER,
+        r"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice",
+        "ProgId",
+    )
+    if browser_id is None:
+        return None
+    browser_cmd = _registry_lookup(
+        HKEY_CLASSES_ROOT, browser_id + r"\shell\open\command"
+    )
+    return browser_cmd
+
+
+def open_with_browser(url: str, browser: str) -> None:
+    """Launch a URL with the given browser
+
+    'browser' should be the launch command for the browser app.
+    it should have '%1' somewhere for input arguments.
+    """
+    browser_cmd = shlex.split(browser)
+    inserted = False
+    for i, field in enumerate(browser_cmd):
+        if "%1" in field:
+            browser_cmd[i] = field.replace("%1", url)
+            inserted = True
+    if not inserted:
+        # no %1, append arg
+        # is this right?
+        browser_cmd.append(url)
+    run(browser_cmd, shell=True)
