041a2d983d57e0f1aab89a52131789610d8bea40
Min RK <benjaminrk@gmail.com>
2025-02-24 10:36:07 +0100

    fewer method calls

--- webbrowser_open/_win.py
+++ webbrowser_open/_win.py
@@ -7,6 +7,20 @@ import sys
 from webbrowser import BaseBrowser
 
 assert sys.platform == "win32"  # for mypy
+import winreg
+
+
+def _registry_lookup(root_key: str, sub_key: str, value_name: str = "") -> str | None:
+    """Lookup a registry item
+
+    Returns None if no value could be read
+    """
+    try:
+        with winreg.OpenKey(root_key, sub_key) as key:
+            return winreg.QueryValueEx(key, value_name)[0]
+    except OSError:
+        return None
+    return None
 
 
 def get_default_browser() -> str | None:
@@ -14,60 +28,46 @@ def get_default_browser() -> str | None:
 
     Returns None if not found
     """
-    import winreg
-
-    browser_id = WindowsDefault._registry_lookup(
+    browser_id = _registry_lookup(
         winreg.HKEY_CURRENT_USER,
         r"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice",
         "ProgId",
     )
     if browser_id is None:
         return None
-    browser_cmd = WindowsDefault._registry_lookup(
+    browser_cmd = _registry_lookup(
         winreg.HKEY_CLASSES_ROOT, browser_id + r"\shell\open\command"
     )
     return browser_cmd
 
 
 class WindowsDefault(BaseBrowser):
-    @staticmethod
-    def _registry_lookup(
-        root_key: str, sub_key: str, value_name: str = ""
-    ) -> str | None:
-        """Lookup a registry item
-
-        Returns None if no value could be read
-        """
-        import winreg
-
-        try:
-            with winreg.OpenKey(root_key, sub_key) as key:
-                return winreg.QueryValueEx(key, value_name)[0]
-        except OSError:
-            return None
-        return None
-
     def _open_default_browser(self, url):
         try:
             import winreg
         except ImportError:
             return False
-        browser_id = self._registry_lookup(
+        with winreg.OpenKey(
             winreg.HKEY_CURRENT_USER,
             r"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice",
-            "ProgId",
-        )
-        if browser_id is None:
-            return False
-        browser_cmd = self._registry_lookup(
-            winreg.HKEY_CLASSES_ROOT, browser_id + r"\shell\open\command"
-        )
-        if browser_cmd is None or "%1" not in browser_cmd:
+        ) as key:
+            browser_id = winreg.QueryValueEx(key, "ProgId")[0]
+        with winreg.OpenKey(
+            winreg.HKEY_CLASSES_ROOT,
+            browser_id + r"\shell\open\command",
+        ) as key:
+            browser_cmd = winreg.QueryValueEx(key, "")[0]
+        if "%1" not in browser_cmd:
+            # don't know how to build cmd
+            # (is append safe?)
             return False
         # this part copied from BackgroundBrowser
         cmdline = [arg.replace("%1", url) for arg in shlex.split(browser_cmd)]
-        p = subprocess.Popen(cmdline)
-        return p.poll() is None
+        try:
+            p = subprocess.Popen(cmdline)
+            return p.poll() is None
+        except OSError:
+            return False
 
     def open(self, url, new=0, autoraise=True):
         sys.audit("webbrowser.open", url)
